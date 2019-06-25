9dd17a06224bb982f719663c1a7a7ca1eebed786
Nguyen Huu Minh <nh_minh@robust-inc.com>
2019-06-25 15:45:28 +0900

    Laravelプロジェクト作成

---
+++ resources/views/home.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Robust Inc.</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Robust Inc.
+        </div>
+
+        <div class="links">
+            <a href="https://www.robust-inc.com/">Home</a>
+            <a href="https://www.robust-inc.com/about.html">About</a>
+            <a href="https://www.robust-inc.com/works.html">Works</a>
+            <a href="https://www.robust-inc.com/recruit.html">Recruit</a>
+            <a href="https://www.robust-inc.com/news.html">News</a>
+            <a href="https://www.robust-inc.com/contact.php">Contact</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
